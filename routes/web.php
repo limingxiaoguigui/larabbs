f44bb0e113e75d7a7a1d9399131e3d20b2285580
LMG <htyzhliminggui@163.com>
2019-05-26 00:40:16 +0000

    个人中心

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+//用户的相关的路由
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');

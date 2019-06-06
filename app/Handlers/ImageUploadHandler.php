fe9ea7f986af5711345088ad73586c0c2f9a3d34
LMG <htyzhliminggui@163.com>
2019-06-06 01:03:11 +0000

    授权访问

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -1,13 +1,15 @@
 <?php
 namespace  App\Handlers;
 
+use  Image;
+
 class  ImageUploadHandler
 {
     //只允许以下后缀的图片上传
     protected   $allowed_ext = ['png', 'jpg', 'gif', 'jpeg'];
 
     //上传方法
-    public  function save($file, $folder, $file_prefix)
+    public  function save($file, $folder, $file_prefix, $max_width = false)
     {
         //构建储存的文件夹规则，uploads/images/avatars/201905/21
         //文件夹切割能让查找效率更高
@@ -27,9 +29,29 @@ class  ImageUploadHandler
         }
         //将图片移到我们的目标从储存路径中
         $file->move($upload_path, $file_name);
+        //如果想限制了图片宽度，就进行裁剪
+        if ($max_width && $extension != 'gif') {
+            //次类中的封装函数用于裁剪图片
+            $this->reduceSize($upload_path . '/' . $file_name, $max_width);
+        }
 
         return [
             'path' => config('app.url') . "/$folder_name/$file_name"
         ];
+    }
+
+    //图片裁剪
+    public  function  reduceSize($file_path, $max_width)
+    {
+        //先实例化，传参文件的磁盘物理路径
+        $image = Image::make($file_path);
+        //进行大小进行调整
+        $image->resize($max_width, null, function ($constraint) {
+            //设定宽度是$max_width,高度等比例双方缩放
+            $constraint->aspectRatio();
+            //防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+        $image->save();
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,15 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+
+    //身份验证
+    public  function  __construct()
+    {
+
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
+
     //个人页展示
     public function  show(User $user)
     {
@@ -17,15 +26,17 @@ class UsersController extends Controller
     //个人资料编辑
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
     //个人资料编辑的处理逻辑
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
         if ($request->avatar) {
 
-            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
 
             if ($result) {
 

99c8dc7315c233dae5552bb5f059f68926556763
LMG <htyzhliminggui@163.com>
2019-06-06 10:17:23 +0000

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,18 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+
+    $sentence = $faker->sentence();
+    //随机取一个月内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+    //时间限制，创建时间永远早于更改时间
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at
     ];
 });
--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,19 +2,34 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
-        });
 
+        //所有用户数组
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        //所有分类
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+        //获取$faker实例
+        $faker = app(Faker\Generator::class);
+        //话题数据
+        $topics = factory(Topic::class)->times(100)
+            ->make()
+            ->each(function ($topic, $index) use ($faker, $user_ids, $category_ids) {
+
+                //从用户数组中随机取出并赋值
+                $topic->user_id = $faker->randomElement($user_ids);
+
+                //随机一个分类
+                $topic->category_id = $faker->randomElement($category_ids);
+            });
+        //将数据转化维数组并插入数据库
         Topic::insert($topics->toArray());
     }
-
 }
-

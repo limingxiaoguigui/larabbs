1c4c9686ba4e62eb77a2de834afe4263ed44fa33
LMG <htyzhliminggui@163.com>
2019-06-09 02:37:21 +0000

    用户可以创建话题

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
     //分类和话题的一对一的关联模型
     public function category()
     {
--- app/helpers.php
+++ app/helpers.php
@@ -1,10 +1,19 @@
 <?php
+
+//返回路由名称
 function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
-
+//判断是否是某个路由
 function  category_nav_active($category_id)
 {
     return  active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
+//制造摘要
+function make_excerpt($value, $lenght = 200)
+{
+    $exceprt = trim(preg_replace('/\r\n|\r|\n+/', '', strip_tags($value)));
+
+    return str_limit($exceprt, $lenght);
 }

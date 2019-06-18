29186abef55fee2966c9fc6ed17fc6d4241bbefc
LMG <htyzhliminggui@163.com>
2019-06-18 08:07:01 +0000

    内容管理后台

--- app/helpers.php
+++ app/helpers.php
@@ -17,3 +17,31 @@ function make_excerpt($value, $lenght = 200)
 
     return str_limit($exceprt, $lenght);
 }
+function  model_admin_link($title, $model)
+{
+    return  model_link($title, $model, 'admin');
+}
+function  model_link($title, $model, $prefix = '')
+{
+    //获取数据模型的复数蛇形命名
+    $model_name = model_plural_name($model);
+    //初始化前缀
+    $prefix = $prefix ? "/$prefix/" : '/';
+    //使用站点的URL拼接全量url
+    $url = config('app.url') . $prefix . $model_name . '/' . $model->id;
+    //拼接HTML 返回
+    return  '<a href="' . $url . '" target="_blank">' . $title . '</a>';
+}
+
+function model_plural_name($model)
+{
+    //从实体中获取完整的类名
+    $full_class_name = get_class($model);
+    //获取基础类名，列如
+    $class_name = class_basename($full_class_name);
+    //蛇形命名
+    $snake_case_name = snake_case($class_name);
+
+    //获取子串的复数形式
+    return  str_plural($snake_case_name);
+}

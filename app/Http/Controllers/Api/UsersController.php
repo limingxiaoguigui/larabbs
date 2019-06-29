d333d8d26d1e98143053218a21f1e46dab40c388
LMG <htyzhliminggui@163.com>
2019-06-29 14:59:52 +0000

    api/user

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\Api\UserRequest;
+use  App\TRANSformers\UserTransformer;
 
 class UsersController extends Controller
 {
@@ -26,6 +27,18 @@ class UsersController extends Controller
         ]);
         //清除验证码缓存
         \Cache::forget($request->verification_key);
-        return  $this->response->created();
+        return  $this->response->item($user, new  UserTransformer())->setMeta([
+            'access_token' => \Auth::guard('api')->fromUser($user),
+            'token_type' => 'Bearer',
+            'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+
+        ])->setStatusCode(201);
+    }
+
+    //获取当前登录用户的信息
+    public  function  me()
+    {
+
+        return  $this->response->item($this->user(), new UserTransformer());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', ['namespace' => 'App\Http\Controllers\Api'], function ($api) {
+$api->version('v1', ['namespace' => 'App\Http\Controllers\Api', 'middleware' => 'serializer:array'], function ($api) {
 
     $api->group(['middleware' => 'api.throttle', 'limit' => config('api.rate_limits.sign.limit'), 'expires' => config('api.rate_limits.sign.expires')], function ($api) {
         //短信验证码
@@ -32,5 +32,17 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api'], function ($api)
         $api->put('authorizations/current', 'AuthorizationsController@update')->name('api.authorizations.update');
         //删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')->name('api.authorizations.destroy');
+    });
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires')
+    ], function ($api) {
+        //有课可以访问的接口
+        //需要token验证的接口
+        $api->group(['middleware' => 'api.auth'], function ($api) {
+            //当前登录用的信息
+            $api->get('user', 'UsersController@me')->name('api.user.show');
+        });
     });
 });

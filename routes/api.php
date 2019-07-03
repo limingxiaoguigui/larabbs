9b9d1a6f3e85ca7c48922ac0aecec180785afd73
LMG <htyzhliminggui@163.com>
2019-07-03 03:45:28 +0000

    话题删除

--- routes/api.php
+++ routes/api.php
@@ -54,6 +54,8 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api', 'middleware' => 
             $api->post('topics', 'TopicsController@store')->name('api.topics.store');
             //修改话题
             $api->patch('topics/{topic}', 'TopicsController@update')->name('api.topics.update');
+            //删除话题
+            $api->delete('topics/{topic}','TopicsController@destroy')->name('api.topics.destroy');
         });
     });
 });

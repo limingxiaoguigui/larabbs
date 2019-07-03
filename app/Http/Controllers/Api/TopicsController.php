085aff57698ea6a524ae236cbe1669409316f1c4
LMG <htyzhliminggui@163.com>
2019-07-03 02:28:46 +0000

    发布话题

---
+++ app/Http/Controllers/Api/TopicsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Models\Topic;
+use App\Transformers\TopicTransformer;
+use App\Http\Requests\Api\TopicRequest;
+
+class TopicsController extends Controller
+{
+    //发布话题
+
+    public  function store(TopicRequest  $request, Topic  $topic)
+    {
+        $topic->fill($request->all());
+        $topic->user_id = $this->user()->id;
+        $topic->save();
+
+        return $this->response->item($topic, new TopicTransformer())->setStatusCode(201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,10 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api', 'middleware' => 
         'limit' => config('api.rate_limits.access.limit'),
         'expires' => config('api.rate_limits.access.expires')
     ], function ($api) {
-        //有课可以访问的接口
+
+        //游客可以访问的接口
+        $api->get('categories','CategoriesController@index')->name('api.categories.index');
+
         //需要token验证的接口
         $api->group(['middleware' => 'api.auth'], function ($api) {
             //当前登录用的信息
@@ -47,6 +50,8 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api', 'middleware' => 
             $api->patch('user', 'UsersController@update')->name('api.user.update');
             //图片资源
             $api->post('images', 'ImagesController@store')->name('api.images.store');
+            //发布话题
+            $api->post('topics','TopicsController@store')->name('api.topics.store');
         });
     });
 });

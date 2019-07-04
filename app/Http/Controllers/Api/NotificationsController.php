3973094d0cc754c3abdd2b0de3c03e6c00ceac93
LMG <htyzhliminggui@163.com>
2019-07-04 10:43:11 +0000

    消息列表

---
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Transformers\NotificationTransformer;
+
+
+class NotificationsController extends Controller
+{
+    //消息通知列表
+    public  function index()
+    {
+
+        $notifications = $this->user->notifications()->paginate(20);
+
+
+        return  $this->response->paginator($notifications, new NotificationTransformer());
+    }
+}
